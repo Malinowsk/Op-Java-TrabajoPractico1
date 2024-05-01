663eeddcf04f6c489a1366bfe21432888549f810
Malinowsk <juanrago2015@gmail.com>
2024-05-01 00:25:13 -0300

    Mejora a la resolucion del ej 1

--- src/main/java/org/example/Ejercicio1/Ejercicio1.java
+++ src/main/java/org/example/Ejercicio1/Ejercicio1.java
@@ -5,6 +5,14 @@ import java.util.Map;
 
 public class Ejercicio1 {
 
+    private static StringBuilder palabraActual;
+    private static int maxRepeticiones;
+    private static String palabraMasRepetida;
+    private static int tamanio;
+    private static Map<String, Integer> contadorPalabras;
+
+/*
+
         public static String contarPalabrasRepetidas(String texto, int tamanio) {
 
             // Dividir el string en palabras individuales
@@ -33,5 +41,51 @@ public class Ejercicio1 {
 
             return palabraMasRepetida;
         }
+
+*/
+
+
+    public static String contarPalabrasRepetidas(String texto, int tamaniomax) {
+        tamanio = tamaniomax;
+        // Crear un mapa para contar las ocurrencias de cada palabra
+        contadorPalabras = new HashMap<>();
+        // Quedarme con la ocurrencias mayor
+        palabraMasRepetida = "";
+        maxRepeticiones = 0;
+        // crear y llevar la palabra actual
+        palabraActual = new StringBuilder();
+
+        for (char c : texto.toCharArray()) {
+            if (Character.isLetter(c)) {
+                palabraActual.append(c);
+            } else {
+                guardarPalabra();
+            }
+        }
+        guardarPalabra();
+        System.out.println(contadorPalabras);
+        return palabraMasRepetida;
+    }
+
+    private static void guardarPalabra() {
+        String palabra = palabraActual.toString().toLowerCase();
+        if (!palabra.isEmpty()) { // palabra actual tiene algo?
+            if (palabra.length() >= tamanio) { // la palabra actual contiene el tamaño necesario para contabilizarla?
+
+                // Contar las ocurrencias de la palabra actual
+                int contador = contadorPalabras.getOrDefault(palabra, 0) + 1;
+
+                // agregar al hashmap
+                contadorPalabras.put(palabra, contador);
+
+                // Actualizar la palabra más repetida si es necesario
+                if (contador > maxRepeticiones) {
+                    maxRepeticiones = contador;
+                    palabraMasRepetida = palabra;
+                }
+            }
+            palabraActual.setLength(0); // Limpiar la palabra actual
+        }
+    }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,22 @@ import org.example.Ejercicio1.Ejercicio1;
 
 public class Main {
     public static void main(String[] args) {
-        String texto = "Hola hola hola mundo mundo mundo mundo hola";
-        int longitudMinimaPalabra = 4; // Definir la longitud mínima de las palabras
+        String texto;
+        int longitudMinimaPalabra; // Definir la longitud mínima de las palabras
 
+        texto = "Hola hola hola mundo mundo mundo mundo hola mmundo  mun peepspel 1443.hola   1hola111hola";
+        longitudMinimaPalabra = 4;
         String palabraMasUsada = Ejercicio1.contarPalabrasRepetidas(texto, longitudMinimaPalabra);
+        System.out.println("La palabra más usada es: " + palabraMasUsada);
+
+        texto = "H h g a f tt yyy y w 23 54y6t6y7u8iooo0 tt 1443.hola.";
+        longitudMinimaPalabra = 1; // Definir la longitud mínima de las palabras
+        palabraMasUsada = Ejercicio1.contarPalabrasRepetidas(texto, longitudMinimaPalabra);
+        System.out.println("La palabra más usada es: " + palabraMasUsada);
+
+        texto = "cursada CURsada OpTaTiVa CURSADA OP.TATIVA";
+        longitudMinimaPalabra = 7; // Definir la longitud mínima de las palabras
+        palabraMasUsada = Ejercicio1.contarPalabrasRepetidas(texto, longitudMinimaPalabra);
         System.out.println("La palabra más usada es: " + palabraMasUsada);
     }
 }
